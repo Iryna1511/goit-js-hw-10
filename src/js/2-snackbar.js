e7d5062dd81a97bd7e1c1cb06ad2776dfeb05383
Iryna1511 <148438865+Iryna1511@users.noreply.github.com>
2024-03-26 16:52:09 +0200

    update

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,7 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const formEl = document.querySelector('.form');
+const elements = {
+  inputEl: formEl.elements.delay,
+};
